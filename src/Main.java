016cb718af4c3c7f9be79f415d2968ba13adae67
SENA <juanmanuelgarciaespinosa@gmail.com>
2024-08-22 10:14:15 -0500

    create menu, create title

--- src/Main.java
+++ src/Main.java
@@ -56,10 +56,10 @@ public class Main {
                                break;
                            case 5:
                                System.out.println("libro completo: \n" +
-                                       "El titulo de la pelicula es: " + Book.getTitle()+ "\n" +
-                                       "El titulo de la pelicula es: " + Book.getEditionDate()+ "\n" +
-                                       "El titulo de la pelicula es: " + Book.getEditionDate()+ "\n" +
-                                       "El titulo de la pelicula es: " + Book.getIsbnsbn() +"\n"
+                                       "El titulo del libro es: " + Book.getTitle()+ "\n" +
+                                       "La fecha de edicion del libro es: " + Book.getEditionDate()+ "\n" +
+                                       "El editorial del libro es: " + Book.getEditionDate()+ "\n" +
+                                       "El isbn del libro es: " + Book.getIsbnsbn() +"\n"
                                );
                                break;
 
@@ -70,8 +70,33 @@ public class Main {
                    break;
 
                 case 2:
-                    System.out.println("Libros que se encuentran en la lista: \n" + Serie.getTitle());
+                    do {
+                        System.out.println("1. title \n 2. genero \n 3. duracion\n 4. serie completa\n");
+                        opcion1 =scanner.nextInt();
+                        switch (opcion1){
+                            case 1:
+                                System.out.println("El titulo de la serie es: " + Serie.getTitle()+ "\n");
+                                break;
+                            case 2:
+                                System.out.println("El genero de la serie es: " + Serie.getGenre()+ "\n");
+                                break;
+                            case 3:
+                                System.out.println("La duracion de la serie es: " + Serie.getDuration()+ "\n");
+                                break;
+                            case 4:
+                                System.out.println("Serie completa: \n" +
+                                        "El titulo de la serie es: " + Serie.getTitle()+ "\n" +
+                                        "El genero de la serie es: " + Serie.getGenre()+ "\n" +
+                                        "La duracion de la serie es: " + Serie.getDuration() +"\n"
+                                );
+                                break;
+
+                        }
+                        break;
+                    }while (opcion1!=5);
+
                     break;
+
                 case 3:
                     System.out.println("Libros que se encuentran en la lista: \n" + Chapter.getTitle());
                     break;
